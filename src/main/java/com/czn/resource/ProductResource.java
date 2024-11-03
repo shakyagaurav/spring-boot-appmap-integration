f834e5f8a3ecd9a01ae6d9257be17878ea115e06
Gaurav Kumar <gauravshakya71@gmail.com>
2024-11-03 20:28:37 +0530

    docker

--- src/main/java/com/czn/resource/ProductResource.java
+++ src/main/java/com/czn/resource/ProductResource.java
@@ -4,12 +4,15 @@ package com.czn.resource;
 import com.czn.entity.Product;
 import com.czn.service.ProductService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1/products")
 @AllArgsConstructor
@@ -19,16 +22,26 @@ public class ProductResource {
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
+        log.info("getAllProducts method invoked");
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 
     @GetMapping("/pagination")
     public ResponseEntity<List<Product>> getAllProducts(@RequestParam(name = "page", defaultValue = "0") int page) {
+        log.info("getAllProducts method invoked for page: {}", page);
         return new ResponseEntity<>(productService.getProductsPagination(page), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<Product> createProduct(@RequestBody Product  product) {
+        log.info("createProduct method invoked for request data: {}", product);
         return new ResponseEntity<>(productService.createProduct(product), HttpStatus.CREATED);
     }
+
+    @GetMapping("/insecure-compare")
+    public ResponseEntity<Boolean> insecureCompare() {
+        log.info("insecureCompare method invoked");
+        return new ResponseEntity<>("czn"=="abc", HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/czn/service/ProductService.java
+++ src/main/java/com/czn/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.czn.service;
 
+import com.appland.appmap.annotation.Labels;
 import com.czn.entity.Product;
 import com.czn.repository.ProductRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepository;
@@ -20,6 +23,7 @@ public class ProductService {
         return productRepository.findAll();
     }
 
+    @Labels({"CreateProductLabel-1"})
     public Product createProduct(Product product) {
 //        String id = String.format("%04d", new Random().nextInt(10000));
 //        return productRepository.save(Product.builder().name("T-Shirt "+id).qty(10).price(Double.valueOf(id)).build());
@@ -29,6 +33,7 @@ public class ProductService {
 
     public List<Product> getProductsPagination(int page) {
         Page<Product> pageProduct = productRepository.findAll(PageRequest.of(page, 10, Sort.Direction.DESC, "price"));
+        log.info("getProductsPagination results: {}", pageProduct);
         return pageProduct.getContent();
     }
 }
